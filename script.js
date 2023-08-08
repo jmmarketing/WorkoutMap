1b891c199f252724af8b2e8140713578a46b4303
jmmarketing <jeff@jeffreymclean.com>
2023-08-08 11:09:49 -0700

    Creating App Class for app methods/functions

--- script.js
+++ script.js
@@ -9,8 +9,7 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-let map;
-let mapEvent;
+let map, mapEvent;
 
 // class Workout {
 //   constructor(id, distance, duration, coords, date) {
@@ -22,56 +21,75 @@ let mapEvent;
 //   }
 // }
 
-// Initializes The Map & Listen for Click on Map
-if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    position => {
-      const { latitude, longitude } = position.coords;
-      console.log(latitude, longitude);
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-      map = L.map('map').setView([latitude, longitude], 13);
+class App {
+  constructor() {
+    // Will Trigger Method On Child Class Creation
+    this._getPosition();
+  }
 
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+  _getPosition() {
+    // Initializes The Map & Listen for Click on Map
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this._loadMap, () =>
+        console.log('Error getting position')
+      );
+    } else {
+      alert('Geolocation not supported');
+    }
+  }
 
-      // Event listener for Clicks on Map
-      map.on('click', function (mapE) {
-        mapEvent = mapE;
-        form.classList.remove('hidden');
-        inputDistance.focus();
-      });
-    },
-    () => console.log('Error getting position')
-  );
-else {
-  alert('Geolocation not supported');
+  _loadMap(position) {
+    const { latitude, longitude } = position.coords;
+    console.log(latitude, longitude);
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    map = L.map('map').setView([latitude, longitude], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    // Event listener for Clicks on Map
+    map.on('click', App.prototype._showForm);
+  }
+
+  _showForm(mapE) {
+    mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {}
+
+  _newWorkout(e) {
+    e.preventDefault();
+
+    // Shows Marker
+    const { lat, lng } = mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          //   content: '🏋🏼‍♀️ Workout',
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('🏀 Workout')
+      .openPopup();
+
+    form.classList.add('hidden');
+  }
 }
 
+const app = new App();
+// app._getPosition();
+
 // Listens for Submit event on Form (Currently only Enter Button)
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  // Shows Marker
-  const { lat, lng } = mapEvent.latlng;
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        //   content: '🏋🏼‍♀️ Workout',
-        className: 'running-popup',
-      })
-    )
-    .setPopupContent('🏀 Workout')
-    .openPopup();
-
-  form.classList.add('hidden');
-});
+form.addEventListener('submit', app._newWorkout);
 
 inputType.addEventListener('change', function () {
   inputElevation.parentElement.classList.toggle('form__row--hidden');
